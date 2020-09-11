d899aaf5db3a021e600eb28e3f44b021b67725c3
YSFish <henryliuw@gmail.com>
2020-09-11 12:56:01 +0800

    update

--- train.py
+++ train.py
@@ -10,31 +10,47 @@ from matplotlib import pyplot as plt
 
 def main():
     # reading in 
-    data_dir = 'data/samples'
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", default='data/sampling', help='determine the base dir of the dataset document')
+    parser.add_argument("--sample_n", default=1000, type=int, help='starting image index of preprocessing')
+    parser.add_argument("--evidence_n", default=20, type=int, help='how many top/bottom tiles to pick from ')
+    args = parser.parse_args()
+    
+    data_dir = args.data_dir
     csv_file = 'data/useful_subset.csv'
+    useful_subset = pd.read_csv(csv_file)
     quick_load = True
     gpu = True
 
-    if quick_load:
+    X_cached_file = os.path.join(data_dir, 'X_%d.pkl' % args.sample_n)
+    Y_cached_file = os.path.join(data_dir, 'Y_%d.pkl' % args.sample_n)
+    if os.path.exists(X_cached_file) and os.path.exists(Y_cached_file):
         print('loading cached data')
-        with open(os.path.join(data_dir, 'X.pkl'),'rb') as file:
+        with open(,'rb') as file:
             X = pickle.load(file)
-        with open(os.path.join(data_dir, 'Y.pkl'),'rb') as file:
+        with open(os.path.join(data_dir, 'Y_%d.pkl' % args.sample_n),'rb') as file:
             Y = pickle.load(file)
     else:
+        print('reading data from preprocessed file')
         useful_subset = pd.read_csv(csv_file)
         Y = useful_subset.loc[0, 'outcome'] == 'good'
-        X = np.loadtxt(data_dir+'/'+'0_features.txt').reshape(1, 1000, 2048)
+        X = np.loadtxt(data_dir+'/'+'0_features.txt').reshape(1, args.sample_n, 2048)
        
         size = len(useful_subset)
         for i in range(1, size):
             X_this = np.loadtxt(data_dir+'/'+str(i)+'_features.txt')
-            if len(X_this)==1000:
-                X = np.r_[X, X_this.reshape(1, 1000, 2048)]
+            if len(X_this)==args.sample_n:
+                X = np.r_[X, X_this.reshape(1, args.sample_n, 2048)]
                 Y = np.r_[Y, useful_subset.loc[i, 'outcome'] == 'good']
             print("\r", "reading data input  %d/%d" % (i, size) , end='', flush=True)
             X = X.transpose((0, 2, 1))
-    
+        
+        with open(X_cached_file,'wb') as file:
+            pickle.dump(X, file)
+        with open(Y_cached_file,'wb') as file:
+            pickle.dump(Y, file)
+
     if gpu:
         X, Y = torch.Tensor(X).cuda(), torch.Tensor(Y).cuda()
     # 5-folds cross validation
@@ -67,7 +83,7 @@ def main():
         acc_fold = None
         early_stop_count = 0
         
-        model = Predictor(evidence_size=20)
+        model = Predictor(evidence_size=args.evidence_n)
         if gpu:
             model = model.cuda()
         optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
@@ -95,7 +111,7 @@ def main():
                     early_stop_count = 0
                 else:
                     early_stop_count += 1
-                if early_stop_count > 2:
+                if early_stop_count > 2 and epoch>200 and auc_fold>0.5:
                     print('early stop at epoch %d' % epoch)
                     break
 
@@ -106,7 +122,7 @@ def main():
         plt.plot(train_history[:, 0], train_history[:, 1], label='train')
         plt.plot(test_history[:, 0], test_history[:, 1], label='test')
         plt.legend()
-        plt.savefig('figure/fold%d.png' % i)
+        plt.savefig('figure/sample_%d_fold%d.png' % (args.sample_n, i))
         plt.cla()
     
     print('CV-auc:%.3f' % sum(auc_folds) / 5)

7b33d7f162c82ee8bc91aff09037ace4fef34b20
lhy <henryliuw@gmail.com>
2020-09-16 21:53:52 +0800

    update dataloader

--- module.py
+++ module.py
@@ -1,7 +1,13 @@
 import torch.nn as nn
 import torch
+import numpy as np
 from sklearn.metrics import roc_auc_score
 from lifelines import CoxPHFitter
+import lifelines
+import warnings
+
+warnings.filterwarnings('ignore') # possibly harmful code
+
 class Predictor(nn.Module):
     def __init__(self, evidence_size=5, layers=[200, 100, 1]):
         # tile scoring
@@ -15,7 +21,7 @@ class Predictor(nn.Module):
             else:
                 linear_layer_list.append(nn.Linear(layers[i-1], layers[i]))
             if (i+1)!=len(layers):
-                linear_layer_list.append(nn.ReLU())
+                linear_layer_list.append(nn.ReLU(inplace=True))
         self.linear_layers = nn.Sequential(*linear_layer_list)
         self.sigmoid_f = torch.nn.Sigmoid()
     def forward(self, image_features):
@@ -30,7 +36,86 @@ class Predictor(nn.Module):
         results = self.sigmoid_f(logits).view(-1)
         return results
 
-def accuracy(result, target):
+def weight_init(m):
+    if isinstance(m, torch.nn.Conv1d) or isinstance(m, torch.nn.Linear):
+        torch.nn.init.kaiming_normal_(m.weight)
+        if m.bias: 
+            torch.nn.init.kaiming_uniform_(m.bias)
+
+class CVDataLoader():
+    ''' a 5-folds cross validation dataloader, with '''
+    def __init__(self, X, Y, df, repl_n, gpu=True, image_split=True):
+        self.gpu = gpu
+        self.X, self.Y = torch.Tensor(X), torch.Tensor(Y)
+        self.df = df
+        self.repl_n = repl_n
+        self.image_split = image_split
+        if image_split:
+            # train test will not have overlapping datapoints from a same image
+            img_idx = df['sample_id'].unique()
+            np.random.shuffle(img_idx)
+            id1, id2, id3, id4 = int(0.2 * len(img_idx)), int(0.4 * len(img_idx)), int(0.6 * len(img_idx)), int(0.8 * len(img_idx))
+            self.perm_idx = np.array((img_idx[:id1], img_idx[id1:id2], img_idx[id2:id3], img_idx[id3:id4], img_idx[id4:]))
+        else:
+            perm = np.random.permutation(len(Y))
+            id1, id2, id3, id4 = int(0.2 * len(Y)), int(0.4 * len(Y)), int(0.6 * len(Y)), int(0.8 * len(Y))
+            self.perm_idx = np.array((perm[:id1], perm[id1:id2], perm[id2:id3], perm[id3:id4], perm[id4:]))
+    
+    def set_fold(self, i):
+        train_idx_slice = [_ for _ in range(5)]
+        train_idx_slice.remove(i)
+        if self.image_split:
+            self.train_idx = self.df['sample_id'].isin(np.concatenate(self.perm_idx[train_idx_slice]))
+            self.test_idx = self.df['sample_id'].isin(self.perm_idx[i])
+        else:
+            self.train_idx = np.concatenate(self.perm_idx[train_idx_slice])
+            self.test_idx = self.perm_idx[i]
+
+        self.idx = [int(len(self.train_idx) * i / self.repl_n) for i in range(self.repl_n)] + [len(self.train_idx)] # for batch
+        self.batch_i = 0
+        self.X_train = self.X[self.train_idx]
+        self.Y_train = self.Y[self.train_idx]
+        if self.image_split:
+            self.df_train = self.df[self.train_idx]
+            self.df_test = self.df[self.test_idx]
+        else:
+            self.df_train = self.df.iloc[self.train_idx]
+            self.df_test = self.df.iloc[self.test_idx]
+
+        self.X_test = self.X[self.test_idx]
+        self.Y_test = self.Y[self.test_idx]
+
+        if self.gpu:
+            self.X_test = self.X_test.cuda()
+            self.Y_test = self.Y_test.cuda()
+
+    def get_test(self):
+        return self.X_test, self.Y_test, self.df_test
+    
+    def get_train(self):
+        if self.gpu:
+            return self.X_train.cuda(), self.Y_train.cuda(), self.df_train
+        else:
+            return self.X_train, self.Y_train, self.df_train
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.batch_i == self.repl_n:
+            raise StopIteration
+        else:
+            train_X_batch = self.X_train[self.idx[self.batch_i]:self.idx[self.batch_i + 1]]
+            train_Y_batch = self.Y_train[self.idx[self.batch_i]:self.idx[self.batch_i + 1]]
+            train_df_batch = self.df_train[self.idx[self.batch_i]:self.idx[self.batch_i + 1]]
+            self.batch_i += 1
+            if self.gpu:
+                train_X_batch = train_X_batch.cuda()
+                train_Y_batch = train_Y_batch.cuda()
+            return train_X_batch, train_Y_batch, train_df_batch
+
+    
+def accuracy(result, target): 
     if result.is_cuda:
         result = result.cpu()
         target = target.cpu()
@@ -45,6 +130,9 @@ def auc(result, target):
 def c_index(result, df):
     if result.is_cuda:
         result = result.cpu()
-    df['predict'] = result.detach().numpy()
-    cph = CoxPHFitter().fit(df, duration_col="time", event_col="y", formula="predict")
-    return cph.concordance_index_
+    #try:
+    #    cph = CoxPHFitter().fit(df, duration_col="time", event_col="y", formula="predict")
+    #except:
+    #    return -1
+    #return cph.concordance_index_
+    return lifelines.utils.concordance_index(df['time'], result.detach().numpy(), ~df['y'])
--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import time
 import os
 import pickle
-from module import Predictor, accuracy, auc
+from module import Predictor, accuracy, auc, c_index, CVDataLoader, weight_init
 from matplotlib import pyplot as plt
 
 def main():
@@ -14,40 +14,46 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_dir", default='data/sampling', help='determine the base dir of the dataset document')
     parser.add_argument("--sample_n", default=1000, type=int, help='starting image index of preprocessing')
-    parser.add_argument("--evidence_n", default=25, type=int, help='how many top/bottom tiles to pick from')
-    parser.add_argument("--repl_n", default=25, type=int, help='how many resampled replications')
+    parser.add_argument("--evidence_n", default=20, type=int, help='how many top/bottom tiles to pick from')
+    parser.add_argument("--repl_n", default=3, type=int, help='how many resampled replications')
+    parser.add_argument("--image_split", action='store_true', help='if use image_split')
     args = parser.parse_args()
-    
-    csv_file = 'data/useful_subset.csv'
-    useful_subset = pd.read_csv(csv_file)
-    useful_subset['OS.time'].fillna(useful_subset['OS.time'].mean(), inplace=True)
+
+    image_split = args.image_split
     gpu = True
     X = None
     Y = None
     X_cached_file = os.path.join(args.data_dir, 'X_%d.npy' % args.sample_n)
     Y_cached_file = os.path.join(args.data_dir, 'Y_%d.npy' % args.sample_n)
-    df_cached_file = os.path.join(args.data_dir, 'df.csv' % args.sample_n)
+    df_cached_file = os.path.join(args.data_dir, 'df.csv')
     if os.path.exists(X_cached_file) and os.path.exists(Y_cached_file):
         print('loading cached data')
         with open(X_cached_file,'rb') as file:
             X = np.load(file)
         with open(Y_cached_file,'rb') as file:
             Y = np.load(file)
-        df_new = pd.read_csv(csv_file)
+        df_new = pd.read_csv(df_cached_file)
     else:
         df_new = None
         print('reading data from preprocessed file')
+        csv_file = 'data/useful_subset.csv'
+        useful_subset = pd.read_csv(csv_file)
+        useful_subset['OS.time'].fillna(useful_subset['OS.time'].mean(), inplace=True)
         size = len(useful_subset)
         for i in range(1, size):
-            for j in range(repl_n):
+            for j in range(args.repl_n):
                 if args.repl_n == 0:
                     X_this = np.loadtxt(args.data_dir+'/'+str(i)+'_features.txt')
                 else:
-                X_this = np.loadtxt(args.data_dir+'/'+str(i)+'_'+str(j)+'_features.txt')
+                    X_this_file = args.data_dir+'/'+str(i)+'_'+str(j)+'_features.txt'
+                    if os.path.exists(X_this_file):
+                        X_this = np.loadtxt(X_this_file)
+                    else:
+                        continue
                 Y_this = useful_subset.loc[i, 'outcome'] == 'good'
                 if len(X_this)==args.sample_n:
                     if X is None:
-                        X = X_this
+                        X = X_this.reshape(1, args.sample_n, 2048)
                     else:
                         X = np.r_[X, X_this.reshape(1, args.sample_n, 2048)]
                     if Y is None:
@@ -55,9 +61,9 @@ def main():
                     else:
                         Y = np.r_[Y, Y_this]
                     if df_new is None:
-                        df_new = pd.DataFrame({"y": Y_this, "time": useful_subset.loc[i, "OS.time"]}, index=[0])
+                        df_new = pd.DataFrame({"y": Y_this, "time": useful_subset.loc[i, "OS.time"], 'sample_id':i}, index=[0])
                     else:
-                        df_new = df_new.append({"y": Y_this, "time": useful_subset.loc[i, "OS.time"]}, ignore_index=True)
+                        df_new = df_new.append({"y": Y_this, "time": useful_subset.loc[i, "OS.time"], 'sample_id':i}, ignore_index=True)
                 print("\r", "reading data input  %d/%d" % (i, size) , end='', flush=True)
         
         X = X.transpose((0, 2, 1))
@@ -70,96 +76,111 @@ def main():
         except Exception as e:
             print(e)
 
-    if gpu:
-        X, Y = torch.Tensor(X).cuda(), torch.Tensor(Y).cuda()
     # 5-folds cross validation
-    perm = np.random.permutation(len(Y))
-    id1, id2, id3, id4 = int(0.2 * len(Y)), int(0.4 * len(Y)), int(0.6 * len(Y)), int(0.8 * len(Y))
-    perm_idx = np.array((perm[:id1], perm[id1:id2], perm[id2:id3], perm[id3:id4], perm[id4:]))
+    dataloader = CVDataLoader(X, Y, df_new, args.repl_n, gpu, image_split)
 
-    n_epoch = 1000
+    n_epoch = 800
     lr = 0.0005
-    weight_decay = 0.05
+    weight_decay = 0.03
     overfit = 0
+    manytimes_n = 2
 
     if not os.path.isdir('figure'):
         os.mkdir('figure')
 
     acc_folds = []
     auc_folds = []
+    c_index_folds = []
+    total_round = 0
 
-    for i in range(5):
-        train_idx_slice = [_ for _ in range(5)]
-        train_idx_slice.remove(i)
-        X_train = X[np.concatenate(perm_idx[train_idx_slice])]
-        Y_train = Y[np.concatenate(perm_idx[train_idx_slice])]
-        X_test = X[perm_idx[i]]
-        Y_test = Y[perm_idx[i]]
-        
-        train_history = []
-        test_history = []
-        minimum_loss = None
-        auc_fold = None
-        acc_fold = None
-        early_stop_count = 0
-        
-        model = Predictor(evidence_size=args.evidence_n, layers=[100, 50, 1])
-        if gpu:
-            model = model.cuda()
-        optimizer = torch.optim.RMSprop(model.parameters(), lr=lr, weight_decay=weight_decay)
-        
-        print('starting fold %d' % i)
-        
-        for epoch in range(n_epoch):
-            result = model(X_train)
-            loss = nn.functional.binary_cross_entropy(result, Y_train)
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-            if epoch % 20 == 0:
-                result_test = model(X_test)
-                loss_test = nn.functional.binary_cross_entropy(result_test, Y_test)
-                train_history.append((epoch, loss, accuracy(result, Y_train), auc(result, Y_train)))
-                test_history.append((epoch, loss_test, accuracy(result_test, Y_test), auc(result_test, Y_test)))
-                print("%s epoch:%d loss:%.3f acc:%.3f auc:%.3f test_loss:%.3f test_acc:%.3f test_auc:%.3f" % 
-                    (time.strftime('%m.%d %H:%M:%S', time.localtime(time.time())), epoch, loss, accuracy(result, Y_train), auc(result, Y_train), loss_test, accuracy(result_test, Y_test), auc(result_test, Y_test)))
-                # early stop
-                if minimum_loss is None or minimum_loss > loss_test:
-                    minimum_loss = loss_test
-                    auc_fold = auc(result_test, Y_test)
-                    acc_fold = accuracy(result_test, Y_test)
-                    early_stop_count = 0
-                elif auc(result_test, Y_test) > auc_fold and auc(result_test, Y_test)>0.5 and accuracy(result_test, Y_test) >= acc_fold:
-                    minimum_loss = loss_test
-                    auc_fold = auc(result_test, Y_test)
-                    acc_fold = accuracy(result_test, Y_test)
-                    early_stop_count = 0
-                else:
-                    early_stop_count += 1
-                if (early_stop_count > 3 and epoch>100 and auc_fold>0.55):
-                    print('early stop at epoch %d' % epoch)
-                    break
-                if overfit == 0 and early_stop_count > 7:
-                    weight_decay *= 2
-                    optimizer = torch.optim.RMSprop(model.parameters(), lr=lr, weight_decay=weight_decay)
-                    overfit = 1
+    for _ in range(manytimes_n):
+        for i in range(5):
+            train_history = []
+            test_history = []
+            minimum_loss = None
+            auc_fold = None
+            acc_fold = None
+            early_stop_count = 0
+            
+            model = Predictor(evidence_size=args.evidence_n, layers=[100, 50, 1])
+            # model.apply(weight_init)
+            if gpu:
+                model = model.cuda()
+            optimizer = torch.optim.RMSprop(model.parameters(), lr=lr, weight_decay=weight_decay)
+            
+            
+            dataloader.set_fold(i)
+            X_test, Y_test, df_test = dataloader.get_test()
+            X_train, Y_train, df_train = dataloader.get_train()
+            print('starting fold %d' % i)
+            
+            for epoch in range(n_epoch):
+                result = model(X_train)
+                loss = nn.functional.binary_cross_entropy(result, Y_train) + nn.functional.mse_loss(result, Y_train)
+                # loss = nn.functional.mse_loss(result, Y_train)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+                
+                # batch input
+                #for X_train_batch, Y_train_batch, df_train_batch in dataloader:
+                #    result = model(X_train_batch)
+                #    loss = nn.functional.binary_cross_entropy(result, Y_train_batch)
+                #    loss.backward()
+                #    optimizer.step()
+                #    optimizer.zero_grad()
 
-        train_history = np.array(train_history)
-        test_history = np.array(test_history)
-        acc_folds.append(acc_fold)
-        auc_folds.append(auc_fold)
-        plt.plot(train_history[:, 0], train_history[:, 1], label='train')
-        plt.plot(test_history[:, 0], test_history[:, 1], label='test')
-        plt.legend()
-        plt.savefig('figure/sample_%d_fold%d.png' % (args.sample_n, i))
-        plt.cla()
-        print("acc:%.3f\tauc:%.3f"  % (acc_fold, auc_fold))
+                if epoch % 25 == 0:
+                    result_test = model(X_test)
+                    loss_test = nn.functional.binary_cross_entropy(result_test, Y_test) + nn.functional.mse_loss(result_test, Y_test)
+                    #loss_test = nn.functional.mse_loss(result_test, Y_test)
+                    acc_train, acc_test = accuracy(result, Y_train), accuracy(result_test, Y_test)
+                    auc_train, auc_test = auc(result, Y_train),  auc(result_test, Y_test)
+                    c_index_train, c_index_test = c_index(result, df_train), c_index(result_test, df_test)
+                    train_history.append((epoch, loss, acc_train, auc_train, c_index_train))
+                    test_history.append((epoch, loss_test, acc_test, auc_test, c_index_test))
+                    print("%s epoch:%d loss:%.3f acc:%.3f auc:%.3f c_index:%.3f test_loss:%.3f test_acc:%.3f test_auc:%.3f test_c_index:%.3f" % 
+                        (time.strftime('%m.%d %H:%M:%S', time.localtime(time.time())), epoch, loss, acc_train, auc_train, c_index_train, loss_test, acc_test, auc_test, c_index_test))
+                    # early stop
+                    if minimum_loss is None or minimum_loss * 0.995 > loss_test:
+                        minimum_loss = loss_test
+                        auc_fold = auc_test
+                        acc_fold = acc_test
+                        c_index_fold = c_index_test
+                        early_stop_count = 0
+                    elif auc_test > auc_fold and auc_test>0.5 and acc_test >= acc_fold:
+                        minimum_loss = loss_test
+                        auc_fold = auc_test
+                        acc_fold = acc_test
+                        c_index_fold = c_index_test
+                        early_stop_count = 0
+                    else:
+                        early_stop_count += 1
+                    if (early_stop_count > 2 and epoch>100 and auc_fold>0.55):
+                        print('early stop at epoch %d' % epoch)
+                        break
+                    if epoch > 500:
+                        optimizer = torch.optim.RMSprop(model.parameters(), lr * 0.8, weight_decay=weight_decay)
+                        overfit = 1
 
-        del X_train, X_test, Y_train, Y_test
-        torch.cuda.empty_cache()
+            train_history = np.array(train_history)
+            test_history = np.array(test_history)
+            acc_folds.append(acc_fold)
+            auc_folds.append(auc_fold)
+            c_index_folds.append(c_index_fold)
+            plt.plot(train_history[:, 0], train_history[:, 1], label='train')
+            plt.plot(test_history[:, 0], test_history[:, 1], label='test')
+            plt.legend()
+            plt.savefig('figure/sample_%d_fold%d.png' % (args.sample_n, i))
+            plt.cla()
+            print("acc:%.3f\tauc:%.3f\tc_index:%.3f"  % (acc_fold, auc_fold, c_index_fold))
+            total_round += 1
+            if gpu:
+                del dataloader.X_train, dataloader.Y_train, dataloader.X_test, dataloader.Y_test
+                del X_test, Y_test, X_train, Y_train, model, optimizer
+                torch.cuda.empty_cache()
     
-    print('CV-auc:%.3f' % (sum(auc_folds) / 5))
-    print('CV-acc:%.3f' % (sum(acc_folds) / 5))
+    print('CV-acc:%.3f CV-auc:%.3f CV-c-index:%.3f' % (sum(acc_folds) / 5 / manytimes_n, sum(auc_folds) / 5 / manytimes_n, sum(c_index_folds) / 5 / manytimes_n))
 
 
 if __name__ == '__main__':
